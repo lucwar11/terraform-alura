6c0f956f2b0fb59f0d2a9547622feb33a68484ca
user <user@users-MacBook-Pro.local>
2023-03-13 00:47:58 -0300

    meu primeiro commit

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0557a15b87f6559cf"
+  instance_type = "t2.micro"
+  key_name = "ec2-primeira"
+
+  tags = {
+    Name = "MinhaPrimeiraInstancia"
+  }
+}
